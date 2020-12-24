7d4560bd61d576631ef0b4bbe01479adeeed07fd
@bjain853 <jainbhavya375@gmail.com>
2020-12-25 03:46:42 +0530

    Can stream movies now too

--- backend/streaming_service/Controllers/MovieActions.js
+++ backend/streaming_service/Controllers/MovieActions.js
@@ -3,6 +3,40 @@ const fs = require("fs");
 const path = require("path");
 //have to add file operations too
 
+const startStream = (path,res,req) => {
+    console.log(path);
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-")
+        const start = parseInt(parts[0], 10)
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize - 1
+
+        const chunksize = (end - start) + 1
+        const file = fs.createReadStream(path, { start, end })
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        }
+
+        res.writeHead(206, head)
+        file.pipe(res)
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        }
+        res.writeHead(200, head)
+        fs.createReadStream(path).pipe(res)
+    }
+
+}
+
 module.exports = {
     create: (request, response) => {
         if (request.body == null || request.body.movieId == null || request.body.file_path == null) {
@@ -56,8 +90,9 @@ module.exports = {
         } else {
             connection.query("SELECT file_path FROM moviefile WHERE movieId = ?", [request.body.movieId], (err, result) => {
                 if (err) throw err;
-                response.json({ file_path: result[0].file_path });
+                startStream(result[0].file_path,response,request);
             })
+            
         }
     }
 
