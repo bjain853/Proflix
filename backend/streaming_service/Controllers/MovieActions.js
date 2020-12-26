17aa62d91f2d54a7e06066250a81d5c9cf4c2492
@bjain853 <jainbhavya375@gmail.com>
2020-12-26 12:54:13 +0530

    deleted update Movie method

--- backend/streaming_service/Controllers/MovieActions.js
+++ backend/streaming_service/Controllers/MovieActions.js
@@ -1,99 +1,70 @@
 const connection = require("../Controllers/connection");
 const fs = require("fs");
 const path = require("path");
-//have to add file operations too
+const startStream = require("../utils/StreamFunctions");
+const { v4: uuidv4 } = require('uuid');
+const utils = require("../utils/fileFunctions");
 
-const startStream = (path, res, req) => {
-    console.log(req.headers.range);
-    const stat = fs.statSync(path)
-    const fileSize = stat.size
-    const range = req.headers.range
-    if (range) {
-        const parts = range.replace(/bytes=/, "").split("-")
-        const start = parseInt(parts[0], 10)
-        const end = parts[1]
-            ? parseInt(parts[1], 10)
-            : fileSize - 1
 
-        const chunksize = (end - start) + 1
-        const file = fs.createReadStream(path, { start, end })
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        }
-
-        res.writeHead(206, head)
-        file.pipe(res)
-    } else {
-        const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4',
-        }
-        res.writeHead(200, head)
-        fs.createReadStream(path).pipe(res)
-    }
-
-}
-
+/**
+ * addMovie adds the movie to database by assigning an id to the movie associated to file and renaming the file to id and adding new file path to db
+ */
 module.exports = {
     addMovie: (request, response) => {
-        if (request.body == null || request.body.movieId == null || request.body.file_path == null) {
+        if (request.body == null || request.body.file_path == null) {
             response.sendStatus(400);
         } else {
+            try {
+                const movieId = uuidv4();
+                console.log(movieId);
+                const newPath = utils.filePathNew(request.body.file_path, movieId, response); // renames file to mId and returns new path
+                connection.query("INSERT INTO moviefile (mId,file_path) VALUES (?,?)", [movieId, newPath], (err, results) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log('Inserted ' + results.affectedRows + ' rows');
+                    response.json({ movieId: movieId });
+                })
+            }
+            catch (error) {
+                response.sendStatus(500);
+            }
 
-            // verify if id is valid by checking using search service
-            var movieId;
-            connection.query("INSERT INTO moviefile (movieId,file_path) VALUES (?,?)", [request.body.movieId, request.body.file_path], (err, results) => {
-                if (err) throw err
-                console.log('Inserted ' + results.affectedRows + ' rows');
-                response.sendStatus(200);
-                movieId = results.insertId;
-            })
-        }
-    },
-    updateMovieById: (request, response) => {
-        if (request.body == null || request.body.movieId == null || request.body.file_path == null) {
-            response.sendStatus(400);
-        } else {
-            connection.query("UPDATE moviefile SET file_path = ? WHERE movieId = ?", [request.body.file_path, request.body.movieId], (err, result) => {
-                if (err) throw err;
-                console.log('Updated ' + result.affectedRows + ' rows');
-                response.sendStatus(200);
+        }},
+        deleteMovieFileById: (request, response) => {
+            if (request.body == null || request.body.movieId == null) {
+                response.sendStatus(400);
+            } else {
+                var fileToDelete;
+                connection.query("SELECT file_path FROM moviefile WHERE mId = ?", [request.body.movieId], (error, result) => {
+                    if (error) throw error;
+                    fileToDelete = result[0].file_path;
+                    fs.unlinkSync(fileToDelete);
+                })
+                connection.query("DELETE FROM moviefile WHERE mId = ?", [request.body.movieId], (err, result) => {
+                    if (err) {
+                        response.sendStatus(500);
+                        throw err;
+                    }
+                    console.log('Deleted ' + result.affectedRows + ' rows');
+                    response.sendStatus(200);
+                });
+            }
 
-            })
-        }
+        },
+            getMovieById: (request, response) => {
+                if (request.body == null || request.body.movieId == null) {
+                    response.sendStatus(400);
+                } else {
+                    connection.query("SELECT file_path FROM moviefile WHERE mId = ?", [request.body.movieId], (err, result) => {
+                        if (err) {
+                            response.sendStatus(500);
+                            throw err;
+                        }
+                        startStream(result[0].file_path, response, request);
+                    })
 
-    },
-    deleteMovieFileById: (request, response) => {
-        if (request.body == null || request.body.movieId == null) {
-            response.sendStatus(400);
-        } else {
-            var fileToDelete;
-            connection.query("SELECT file_path FROM moviefile WHERE movieId = ?", [request.body.movieId], (error, result) => {
-                if (err) throw err;
-                fileToDelete = result[0].file_path;
-            })
-            connection.query("DELETE FROM moviefile WHERE movieId = ?", [request.body.movieId], (err, result) => {
-                if (err) throw err;
-                console.log('Deleted ' + result.affectedRows + ' rows');
-                response.sendStatus(200);
-            });
-            fs.unlinkSync(fileToDelete);
-        }
+                }
+            }
 
-    },
-    getMovieById: (request, response) => {
-        if (request.body == null || request.body.movieId == null) {
-            response.sendStatus(400);
-        } else {
-            connection.query("SELECT file_path FROM moviefile WHERE movieId = ?", [request.body.movieId], (err, result) => {
-                if (err) throw err;
-                startStream(result[0].file_path, response, request);
-            })
-
-        }
     }
-
-}
--- backend/streaming_service/routes/MovieRoutes.js
+++ backend/streaming_service/routes/MovieRoutes.js
@@ -5,7 +5,7 @@ const MovieActions = require("../Controllers/MovieActions");
 
 router.get('/getMoviebyId', (request, response) => { MovieActions.getMovieById(request, response) });
 router.post('/addMovie', (request, response) => { MovieActions.addMovie(request, response) });
-router.put('/updateMovieById', (request, response => { MovieActions.updateMovieById(request, response) }));
-router.delete('/movieById', (request, response) => { MovieActions.deleteMovieFileById(request, response) });
+//router.put('/updateMovieById', (request, response => { MovieActions.updateMovieById(request, response) }));
+router.delete('/deleteMovieById', (request, response) => { MovieActions.deleteMovieFileById(request, response) });
 
 module.exports = router;
